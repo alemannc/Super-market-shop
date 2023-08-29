f24905e63e783d73a7b77a4973a1e2d42f4bc677
Nicolas Castellano <felipenicolas115@gmail.com>
2023-08-29 17:51:41 -0300

    Ruta,handler y controller byId de orderDetail

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3001;
 const { conn } = require('./src/db');
 
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
