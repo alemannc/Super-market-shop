d6cb7a4fb478f91d57782b97af531010bbf27dd9
Nicolas Castellano <felipenicolas115@gmail.com>
2023-09-02 13:02:06 -0300

    Inicio de mercado pago y correcion de scrip start para deploy

--- api/src/Routes/mercadoPagoRoutes.js
+++ api/src/Routes/mercadoPagoRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mercadoPagoRouter = express.Router();
+const mercadoPagoRouter =express.Router();
 const postOrderMP = require ("../Handlers/mercadoPagoHandler/createOrderHandler")
 
 mercadoPagoRouter.post("/createOrder", (req,res)=>{
@@ -7,7 +7,7 @@ mercadoPagoRouter.post("/createOrder", (req,res)=>{
 });
 
 mercadoPagoRouter.get("/:id" ,(req,res)=>{
-    getDetailById(req,res)
+    
 });
 
 
