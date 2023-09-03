5077423eab57b91603ea7c2aca396f42d9798305
JuanManuelA12 <aguirre.juanmanuel029@gmail.com>
2023-09-03 16:13:53 -0400

    rutagetproductbyid

--- api/src/Routes/index.js
+++ api/src/Routes/index.js
@@ -3,6 +3,7 @@ const customerRouter = require('./customerRoutes');
 const orderDetailRouter = require("./orderDetailRoutes")
 const productRoutes = require("./productRoutes");
 const getallProducts = require('./productRoutes');
+const findProductById = require('./productRoutes');
 
 
 const router = Router();
@@ -11,7 +12,7 @@ router.use('/customer', customerRouter);
 router.use("/orderDetail",orderDetailRouter)
 router.use("/product", productRoutes);
 router.use("/getallProducts", getallProducts );
-
+router.use("/product/:id", findProductById );
 
 module.exports = router;
 //asdasd
--- api/src/Routes/productRoutes.js
+++ api/src/Routes/productRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const productRouter =express.Router();
 const postProduct = require("../Handlers/ProductHandler/postProduct.js");
 const getallProducts = require("../Handlers/ProductHandler/getallProducts.js");
+const findProductById = require("../Handlers/ProductHandler/findProductById.js")
 
 productRouter.post('/',(req,res)=>{
     postProduct(req,res);
@@ -9,6 +10,10 @@ productRouter.post('/',(req,res)=>{
     
 productRouter.get('/',(req,res)=>{
     getallProducts(req,res);
+});
+
+productRouter.get('/:id',(req,res)=>{
+    findProductById(req,res);
 });
     
 
