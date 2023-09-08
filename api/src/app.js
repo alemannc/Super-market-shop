fa6fa85e35bf9f9e0dce94532f9502717e9706ae
MatiasIvanM <matiasivan.sd@gmail.com>
2023-09-08 20:58:27 -0300

    Agregado de cors para deploy front

--- api/src/app.js
+++ api/src/app.js
@@ -2,13 +2,15 @@ const express = require("express");
 require("dotenv").config();
 const morgan = require('morgan');
 const routes = require('./Routes/index');
+const cors = require('cors');
 
 const server = express();
 
 
 server.use(express.json());
+server.use(cors());
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+  res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
