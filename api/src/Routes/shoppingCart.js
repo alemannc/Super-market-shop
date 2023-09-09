b1e083b8a2b3675d7daf20ae08ae0bce363cb784
Nicolas Castellano <felipenicolas115@gmail.com>
2023-09-09 11:40:58 -0300

    ruta byid shopping cart

--- api/src/Routes/shoppingCart.js
+++ api/src/Routes/shoppingCart.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const shoppingCartRouter =express.Router();
 const postShoppingCart = require ("../Handlers/shoppingCartHandler/postSCHandler")
+const getShoppingCartById= require("../Handlers/shoppingCartHandler/ByIdSCHandler")
 
 shoppingCartRouter.post('/',(req,res)=>{
     postShoppingCart(req,res);
 });
 
-
+shoppingCartRouter.get('/:id', (req,res)=>{
+    getShoppingCartById(req,res);
+});
 
 
 module.exports= shoppingCartRouter;
