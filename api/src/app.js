e7d48c4dd49b0c2dd2a8c8b7478e93ed3e957424
ALoyoVasquez <anitamlv@gmail.com>
2023-09-09 11:38:39 -0400

    cam

--- api/src/app.js
+++ api/src/app.js
@@ -2,14 +2,17 @@ const express = require("express");
 require("dotenv").config();
 const morgan = require('morgan');
 const routes = require('./Routes/index');
-
+const cors = require("cors");
 const server = express();
 
 
 server.use(express.json());
+server.use(cors());
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
   res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Content-Range", "product 0-10/10");   //Agregado para el dashboard
+  res.header("Access-Control-Expose-Headers", "Content-Range");//Agregado para el dashboard
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
